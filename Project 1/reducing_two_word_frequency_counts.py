6c8d22ad9a3c5b296bd8680a574c4276eb0b9937
Natarajan Chidhambharam <natrajct@gmail.com>
2018-04-02 15:28:13 -0700

    Add files via upload

---
+++ Project 1/reducing_two_word_frequency_counts.py
@@ -0,0 +1,75 @@
+# CS 221 - Assignment 1 - Part C: Reducing two word-frequency counts
+# FileName: 75003249_partC.py
+#
+# Student Name: Natarajan Chidhambharam
+# Student No: 75003249
+# 
+# The program takes two word-frequency maps and creates an output word-frequency count file where the counts are added
+#
+# Execution: python 75003249_partC.py <Input-file 1> <Input-file 2>
+#
+# References: https://stackoverflow.com/questions/5622976/how-do-you-calculate-program-run-time-in-python
+#             https://stackoverflow.com/questions/34156822/python-sort-dictionary-items-by-value-and-then-key
+
+import sys 
+import re
+import timeit
+
+def printWordFreqCount(Frequencies,FileToWriteOutput):
+    #Write to output file
+    with open(FileToWriteOutput,'w') as file:
+        #To sort the key-value pairs in the 'Frequencies' dictionary in alphabetical order:
+        for key,value in sorted(Frequencies.items()):
+            file.write("%s , %d\n" %(key,value))
+        
+
+def main():
+	# Checking the execution of the script - if the number of arguments is right
+	if (len(sys.argv) != 3):
+		print('Check the number of arguments. Execute as python 75003249_partC.py <Input-file 1> <Input-file 2>')
+		sys.exit()
+	else:
+		#Getting the input file names
+		TextFilePath_1 = sys.argv[1]
+        	TextFilePath_2 = sys.argv[2]
+
+        	#Output File name with path
+        	OutputFileName = "partC_output.txt"
+
+        	#Initializing a dictionary to store the token-frequency pairs from the input file
+        	Dict_1 = {}
+
+        	#Reading the input file 1
+        	with open(TextFilePath_1,"r") as file:
+            		for x in file:
+                		#Splitting the line and converting all the words into lowercase
+                		x = x.lower().split(',')
+                		#Storing the values to the corresponding keys in the dictionary - converting the word-frequency counts as integer values
+                		Dict_1[x[0]] = int(x[1])
+
+        	#Reading the input file 2
+        	with open(TextFilePath_2,"r") as file:
+            		for x in file:
+                		#Splitting the line and converting all the words into lowercase
+                		x = x.lower().split(',')
+                		#Storing the values to the corresponding keys in the dictionary - converting the word-frequency counts as integer values
+                		#If the new key is already in the first input file, then add the new value to the corresponding value or append the new key-value pair to the dictionary
+                		if x[0] in Dict_1:
+                    			Dict_1[x[0]] += int(x[1])
+                		else:
+                    			Dict_1[x[0]] = int(x[1])
+
+        	#Printing the updated dictionary to the output file partC_output.txt
+        	printWordFreqCount(Dict_1,OutputFileName)
+
+#Calculating the start time
+start = timeit.default_timer()
+
+#Executing the main fucntion
+main()
+
+#Calculating the end time
+stop = timeit.default_timer()
+
+#Printing the total time taken by the program
+print 'Total time - ',stop - start,'sec'
---
+++ Project 1/word_frequencies.py
@@ -0,0 +1,90 @@
+# CS 221 - Assignment 1 - Part A: Word Frequencies
+# FileName: 75003249_partA.py
+#
+# Student Name: Natarajan Chidhambharam
+# Student No: 75003249
+# 
+# Functions Implemented:
+# 		List<Token> tokenize(TextFilePath)
+# 		Map<Token,Count> computeWordFrequencies(List<Token>)
+# 		void print(Frequencies<Token,Count>)
+#
+# Execution: python 75003249_partA.py <Input-file name>
+#
+# References: https://stackoverflow.com/questions/5622976/how-do-you-calculate-program-run-time-in-python
+#			  https://stackoverflow.com/questions/5733419/how-to-iterate-over-the-file-in-python
+#             https://stackoverflow.com/questions/5843518/remove-all-special-characters-punctuation-and-spaces-from-string/23853882
+#			  https://stackoverflow.com/questions/4546879/which-one-to-prefer-line-lower-vs-word-lower	
+#			  https://stackoverflow.com/questions/34156822/python-sort-dictionary-items-by-value-and-then-key
+
+import sys 
+import re
+import timeit
+
+def tokenize(TextFilePath):
+    
+	#Declaring 'Token' as a list
+    Token = []
+
+    #Open input file for reading
+    with open(TextFilePath,"r") as file:
+        for x in file:
+        	#Replacing special characters with a space for tokenization
+            x = re.sub('\W',' ',x)
+            #Splitting the line and putting all the words in Token list as lowercase
+            for word in x.lower().split():
+                Token.append(word)
+
+    #Returning the Token list            
+    return Token
+
+def computeWordFrequencies(Token):
+
+	#Declaring 'Frequencies' as a dictionary (key, value) to compute count
+    Frequencies = {}
+
+    #Iterate through the Token list
+    for word in Token:
+        if word in Frequencies:
+            Frequencies[word] += 1
+        else:
+            Frequencies[word] = 1
+    
+    #Return the word frequencies as a key value pair
+    return Frequencies
+
+def printWordFreqCount(Frequencies):
+
+	#To sort the key-value pairs in the 'Frequencies' dictionary from highest to lowest:
+    for key,value in sorted(Frequencies.items(),key=lambda x:x[1],reverse = True):
+        print("%s - %s" %(key,value))
+
+def main():
+	# Checking the execution of the script - if the number of arguments is right
+	if (len(sys.argv) != 2):
+		print("Check the number of arguments. Execute as python 75003249_partA.py <Input-file name>")
+		sys.exit()
+	else:
+		#Getting the input file name
+		TextFilePath = sys.argv[1]
+
+    		#Tokenize the input file
+		Token = tokenize(TextFilePath)
+
+		#Compute the word frequncies from the Token list
+		Frequencies = computeWordFrequencies(Token)
+
+		#Print the word frequency counts
+    	printWordFreqCount(Frequencies)
+
+#Calculating the start time
+start = timeit.default_timer()
+
+#Executing the main fucntion
+main()
+
+#Calculating the end time
+stop = timeit.default_timer()
+
+#Printing the total time taken by the program
+print 'Total time - ',stop - start,'sec'
